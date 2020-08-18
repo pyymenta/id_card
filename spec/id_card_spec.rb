f3f57afd52c007ef89a70830ba907224785a4468
Vanessa Queiroz <vanessacaqz@gmail.com>
2020-08-18 18:58:15 -0300

    first commit

---
+++ spec/id_card_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe do
+  def generate_id(name)
+  
+  end
+
+  it 'does something' do
+    name = "Jigarius Caesar"
+    id = generate_id(name)
+
+    expect(id).to be 'CAJI20200296'
+  end
+end
